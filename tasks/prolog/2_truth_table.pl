0b77108cad3fb24df6465439a63aa91c4517a5a8
kochuv <veronika.kochugova@gmail.ru>
2019-11-15 19:02:28 +0300

    Second task done.

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,48 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+
+% --------------------------------------------------------
+% my functions:
+%
+
+
+and(A, B) :- A, B.
+or(A, B)  :- A; B.
+xor(A, B) :- A \= B.
+not(A)    :- \+ A.
+equ(A, B) :- A = B.
+
+bool(true).
+bool(false).
+
+evaluate(E, true) :- E, !.
+evaluate(_, false).
+
+truth_table(A,B,E) :-
+  bool(A),
+  bool(B),
+  write(A),
+  write(' \t '),
+  write(B),
+  write(' \t '),
+  evaluate(E, Result),
+  write(Result),
+  nl,
+  fail.
+
+% --------------------------------------------------------
+% my commnads to check:
+%
+
+?- truth_table(A, B, and(A, or(A, not(B)))).
+
+% --------------------------------------------------------
+% My output:
+%
+
+% true     true    true
+% true     false   true
+% false    true    false
+% false    false   false
